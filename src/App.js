ed805c6d29449d951630419f013ad700c7b9b7be
oderathegreat <oderathegreat@gmail.com>
2023-09-08 14:43:16 +0300

    code clean up implementing in a new strategy

--- src/App.js
+++ src/App.js
@@ -2,42 +2,17 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
-  const url = 'https://restcountries.com/v3.1/all';
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const result = await response.json();
-        setIsLoaded(true);
-        setItems(result);
-      } catch (error) {
-        setError(error);
-      }
-    };
-
-    fetchData();
-  }, []);
+ 
 
   return (
     <div className="App">
-      {error ? (
-        <div>Error: {error.message}</div>
-      ) : !isLoaded ? (
-        <div>Loading...</div>
-      ) : (
-        <ul>
-          {items.map((item) => (
-            <li key={item.name}>{item.name}</li>
-          ))}
-        </ul>
-      )}
+<div className="searchbarcontainer">
+
+  <div>Search Bar</div>
+
+  <div>Search Bar Result </div>
+</div>
+
     </div>
   );
 }
---
+++ src/intialcall.js
@@ -0,0 +1,50 @@
+
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  const url = 'https://restcountries.com/v3.1/all';
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        // setIsLoaded(true);
+        // setItems(result);
+
+
+        console.log(result)
+        
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      {error ? (
+        <div>Error: {error.message}</div>
+      ) : !isLoaded ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {items.map((item) => (
+            <li key={item.name}>{item.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default App;
