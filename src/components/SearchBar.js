7f652a37e3ef768aa1a909222754df339ca69f75
oderathegreat <oderathegreat@gmail.com>
2023-09-09 10:26:19 +0300

    search component to fetch data

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {FaSearch} from "react-icons/fa";
 import "./Searchbar.css";
 
 
 const SearchBar = () => {
+
+  const [input, setInput] = useState("");
+
+  //fetch data fro external api
+  const fetchData = (value) => {
+    //call endpoint get the response and convert to json
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then( (response)=> response.json())
+      .then((json)=>{
+        console.log(json)
+      })
+
+
+  }
+
+  //function to handle changes in value
+  const handleChange = (value) =>{
+
+    setInput(value)
+    fetchData(value)
+  }
+
+
+
   return (
     <div className='input-wrapper'>
 
         <FaSearch id="search-icon"/ >
-            <input  placeholder= "Type to search names" />
+            <input  placeholder= "Type to search names"
+            value={input}
+            onChange={ (e)=> handleChange(e.target.value)}
+            
+            
+            />
+
+            <div>
+
+              
+            </div>
 
        
       
